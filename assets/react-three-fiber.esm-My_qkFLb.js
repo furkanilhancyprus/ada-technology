e4c482b81ab73250803fdf4b3f57b19a6e9e1699
Furkan İlhan <furkanilhancyprus@gmail.com>
2026-06-01 09:33:34 +0300

    Deploy project link updates

--- assets/react-three-fiber.esm-My_qkFLb.js
+++ assets/react-three-fiber.esm-My_qkFLb.js
@@ -1,4 +1,4 @@
-import{r as ze,g as I0}from"./index-C8jqjEDA.js";/**
+import{r as ze,g as I0}from"./index-3J8EEOQD.js";/**
  * @license
  * Copyright 2010-2023 Three.js Authors
  * SPDX-License-Identifier: MIT
